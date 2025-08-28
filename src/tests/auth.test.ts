cc042041d061843135f58b3a630eab80304e5b59
turneyandhall <richard@turneyandhall.com>
2025-08-28 07:32:46 +0100

    add Vitest

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+
+describe("auth", () => {
+  test("no headers", () => {
+    expect(getAPIKey({})).toBeNull();
+  });
+
+  test("has header", () => {
+    expect(
+      getAPIKey({
+        authorization: "ApiKey 123abc",
+      })
+    ).toBe("1234abc");
+  });
+});
